f152d9fd37efd5ec53bebb975f5d696652e09264
astel <anapchela@gmail.com>
2025-10-28 17:13:51 +0300

    update

--- source/conf.py
+++ source/conf.py
@@ -24,7 +24,9 @@ language = 'ru'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# html_theme = 'shibuya'
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'shibuya'
+# html_theme = 'sphinx_rtd_theme'
 
 html_static_path = ['_static']
+html_extra_path = ['_build/html']
+html_base_url = '/networks_sphinx/'
